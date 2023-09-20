4d3a0e9536d2effd567ff79b13181f04f0e71267
Chette <80092129+MrChette@users.noreply.github.com>
2023-09-20 19:42:36 +0200

    Section 3 : Resful Web Services with Spring Boot - V2 (11-26)

---
+++ RestFul Web Services with Spring Boot - V2/restful-web-services/src/main/java/com/spring/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,46 @@
+package com.spring.rest.webservices.restfulwebservices.user;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class UserDaoService {
+	
+	//Implements methods over the users that will talk to the BBDD
+	
+	
+	private static List<User> users = new ArrayList<User>();
+	
+	public static int usersCount = 0;
+	
+	static {
+		users.add(new User(++usersCount,"Nacho",new Date()));
+		users.add(new User(++usersCount,"Javi",new Date()));
+		users.add(new User(++usersCount,"Paco",new Date()));
+	}
+	
+	
+	public List<User> findAll() {
+		return users;
+	}
+	
+	public User findOne(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id); 
+		return users.stream().filter(predicate).findFirst().orElse(null);
+	}
+	
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
+	}
+	
+	
+
+}
