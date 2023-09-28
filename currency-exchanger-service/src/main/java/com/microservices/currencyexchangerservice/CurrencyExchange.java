a3b7b0d46fed98a1f5fe2cf63f111911299ec532
Chette <80092129+MrChette@users.noreply.github.com>
2023-09-28 14:20:50 +0200

    Section 6 : Microservices with Spring Cloud (138-143)
    Simple currency exchange service using differents ports and runnin in different configurations in eclipse, im also using loombok to implements constructors, getters, setters and tostring methods, h2 implemented with some sql functions

---
+++ currency-exchanger-service/src/main/java/com/microservices/currencyexchangerservice/CurrencyExchange.java
@@ -0,0 +1,50 @@
+package com.microservices.currencyexchangerservice;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+
+@Entity
+public class CurrencyExchange {
+	
+	@Id
+	private Long id;
+	
+	@Column(name = "currency_from")
+	private String from;
+	
+	@Column(name = "currency_to")
+	private String to;
+	
+	@Column(nullable = false, updatable = false)
+	private BigDecimal conversionMultiple;
+	
+	@Column(nullable = false, updatable = false)
+	private String environment;
+	
+	// ArgsConstructor without environment
+	public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+	}
+	
+	
+	
+
+}
