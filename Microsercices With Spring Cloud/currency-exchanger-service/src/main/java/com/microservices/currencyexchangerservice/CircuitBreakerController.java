2cc6bd40a8de6a71602a98dc895326845808304d
Chette <80092129+MrChette@users.noreply.github.com>
2023-10-04 21:02:29 +0200

    Section 6 : Microservices with Spring Cloud (166-167) (FIN)
    Circuit Braker( RateLimiter & BulkHead)
    application.properties of Resilience4j

--- Microsercices With Spring Cloud/currency-exchanger-service/src/main/java/com/microservices/currencyexchangerservice/CircuitBreakerController.java
+++ Microsercices With Spring Cloud/currency-exchanger-service/src/main/java/com/microservices/currencyexchangerservice/CircuitBreakerController.java
@@ -2,12 +2,10 @@ package com.microservices.currencyexchangerservice;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 
 @RestController
 public class CircuitBreakerController {
@@ -15,14 +13,19 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 	
 	@GetMapping("/sample-api")
-	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	//@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	//@CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	//@RateLimiter(name = "default") // Example in 10s => only 10000calls
+	@Bulkhead(name = "sample-api")
 	public String sampleApi() {
 		
-		logger.info("Sample API call received");
+//		logger.info("Sample API call received");
+//		
+//		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-nothing", String.class);
+//		
+//		return forEntity.getBody();
 		
-		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-nothing", String.class);
-		
-		return forEntity.getBody();
+		return "Sample API";
 	}
 	
 	public String hardcodedResponse(Exception ex) {
